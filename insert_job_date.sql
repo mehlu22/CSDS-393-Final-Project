f0c6c60d5e508d8a8a9f2270212e1e9f4c0c10ea
Jacob Hall <129861000+Geargoblin@users.noreply.github.com>
2024-04-01 18:39:27 -0400

    Create insert_job_data.sql
    Insert data to job table

---
+++ insert_job_date.sql
@@ -0,0 +1,3 @@
+INSERT INTO US_JOB_DATA (City, Job, Exp_Sal, Role_Desc, Role, Type) VALUES
+	(‘Chicago’, ‘Artist’, ‘50000’, NULL, ‘Entry Level’, ‘Remote’),
+	(‘Chicago’, ‘Doctor’, ‘130000, ‘Heal People’, ‘Senior Level’, ‘In Person’);
