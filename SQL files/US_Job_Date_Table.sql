21177ad97c2e03d8428559b8fbb75edef31103ee
Jacob Hall <129861000+Geargoblin@users.noreply.github.com>
2024-04-07 21:28:08 -0400

    Update US_Job_Date_Table.sql
    Updated table to fit new model.

--- SQL files/US_Job_Date_Table.sql
+++ SQL files/US_Job_Date_Table.sql
@@ -1,11 +1,8 @@
 CREATE TABLE ‘US_JOB_DATA’ (
 	‘City’ VARCHAR(300) NOT NULL,
-	‘Job’ VARCHAR(20) NOT NULL,
+	‘Organization’ VARCHAR(20) NOT NULL,
 	‘Exp_Sal’ INT NOT NULL,
-	‘Role_Desc’ VARCHAR(200),
-	‘Role’ VARCHAR(12),
 	‘Type’ VARCHAR(9),
-	PRIMARY KEY (‘City’, ‘Job’)
-	Check ‘Role’ in {‘Entry Level’, ‘Mid Level’, ‘Senior Level’}
+	PRIMARY KEY (‘City’, ‘Organization’)
 	Check ‘Type’ in {‘Remote’, ‘Hybrid’, ‘In Person’}
 );
