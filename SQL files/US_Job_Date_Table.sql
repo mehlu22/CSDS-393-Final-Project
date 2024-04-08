41842067e5862e8cc8cdb2f4128efdb6332a4965
Jacob Hall <129861000+Geargoblin@users.noreply.github.com>
2024-04-07 21:29:46 -0400

    Update US_Job_Date_Table.sql
    Fixed table syntax

--- SQL files/US_Job_Date_Table.sql
+++ SQL files/US_Job_Date_Table.sql
@@ -1,8 +1,8 @@
-CREATE TABLE ‘US_JOB_DATA’ (
-	‘City’ VARCHAR(300) NOT NULL,
-	‘Organization’ VARCHAR(20) NOT NULL,
-	‘Exp_Sal’ INT NOT NULL,
-	‘Type’ VARCHAR(9),
-	PRIMARY KEY (‘City’, ‘Organization’)
-	Check ‘Type’ in {‘Remote’, ‘Hybrid’, ‘In Person’}
+CREATE TABLE US_JOB_DATA (
+    City VARCHAR(300) NOT NULL,
+    Organization VARCHAR(20) NOT NULL,
+    Exp_Sal INT NOT NULL,
+    Type VARCHAR(9),
+    PRIMARY KEY (City, Organization),
+    CONSTRAINT Type_check CHECK (Type IN ('Remote', 'Hybrid', 'In Person'))
 );
