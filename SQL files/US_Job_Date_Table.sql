5ea21cff959d3ec69cdd25ba7f4220f43009f829
mehlu22 <mehlu22@gmail.com>
2024-04-11 16:34:12 -0400

    update database

--- SQL files/US_Job_Date_Table.sql
+++ SQL files/US_Job_Date_Table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE US_JOB_DATA (
-    City VARCHAR(300) NOT NULL,
-    Organization VARCHAR(100) NOT NULL,
+    City VARCHAR(500) NOT NULL,
+    Organization VARCHAR(500) NOT NULL,
     Exp_Sal INT NOT NULL,
-    Type VARCHAR(9),
+    Type VARCHAR(19),
     PRIMARY KEY (City, Organization),
     CONSTRAINT Type_check CHECK (Type IN ('Remote', 'Hybrid', 'In Person'))
 );
