0e98e237240377af839ba0c91195ec3f27922b50
mehlu22 <mehlu22@gmail.com>
2024-04-11 16:00:14 -0400

    change database

--- SQL files/US_Job_Date_Table.sql
+++ SQL files/US_Job_Date_Table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE US_JOB_DATA (
     City VARCHAR(300) NOT NULL,
-    Organization VARCHAR(20) NOT NULL,
+    Organization VARCHAR(100) NOT NULL,
     Exp_Sal INT NOT NULL,
     Type VARCHAR(9),
     PRIMARY KEY (City, Organization),
