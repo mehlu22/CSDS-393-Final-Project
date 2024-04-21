3a4c38fdd5e99694941ab05e7d23666195013e9a
Vishwa78 <vishwaaduru@gmail.com>
2024-04-21 13:17:11 -0400

    Update housing_utils.py

--- housing_utils.py
+++ housing_utils.py
@@ -14,3 +14,24 @@ SELECT City, CostOfLiving, CrimeRate, PublicTransportation, EnvironmentalRating,
 FROM us_neighborhood_data
 """
 data = pd.read_sql(query, engine)
+
+def predict_top_localities(city, features, n=2):
+    """    
+    Parameters:
+    city: str - The city from which to filter localities.
+    features: list - A list containing the features in the following order:
+                      [CostOfLiving, CrimeRate, PublicTransportation, EnvironmentalRating, PublicSchoolRating]
+    n: int - Number of top localities to return
+
+    Returns:
+    list: Predicted top 2 locality names
+    """
+    city_data = data[data['City'].str.lower() == city.lower()]
+    if city_data.empty:
+        return ["No data available for this city."]
+    
+    X_city = city_data[['CostOfLiving', 'CrimeRate', 'PublicTransportation', 'EnvironmentalRating', 'PublicSchoolRating']]
+    y_city = city_data['Locality']
+
+    X_train, _, y_train, _ = train_test_split(X_city, y_city, test_size=0.3, random_state=42)
+
