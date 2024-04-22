36af29ae0190bfe165690bc11bdc6cf3442f7466
GBK2002 <66865678+GBK2002@users.noreply.github.com>
2024-04-22 02:10:12 -0400

    app.js bug fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,7 +29,7 @@ function App() {
     const handleCityDetailsClose = () => setIsCityDetailsOpen(false);
     const handleCompanyDialogClose = () => setIsCompanyDialogOpen(false);
     const handleNeighborhoodDialogClose = () => setIsNeighborhoodDialogOpen(false);
-    const handleNeighborhoodListDialogClose = () => setIsNeighborhoodListOpen(false);
+    const handleNeighborhoodListClose = () => setIsNeighborhoodListOpen(false);
 
     
 
