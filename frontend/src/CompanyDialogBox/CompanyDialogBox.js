3b67a8212bae6f6730f6f635b73b33d726e94682
GBK2002 <66865678+GBK2002@users.noreply.github.com>
2024-04-21 21:05:26 -0400

    Company Dialog Box update for integration

--- frontend/src/CompanyDialogBox/CompanyDialogBox.js
+++ frontend/src/CompanyDialogBox/CompanyDialogBox.js
@@ -1,23 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import './CompanyDialogBox.css'; // Make sure to create and link the CSS file
 
-function CompanyDialogBox({ onClose }) {
-  const [companies, setCompanies] = useState([]);
+function CompanyDialogBox({company, onClose, onSelectCompany }) {
 
   useEffect(() => {
-    pickRandomCompanies();
   }, []);
 
-  const pickRandomCompanies = () => {
-    const allCompanies = ['Amazon', 'UHS', 'Apple', 'IDR', 'Fortis Hospital', 'Zuckerberg International Hospital', 'Blackrock', 'Meta', 'Goldman Sachs'];
-    let shuffled = allCompanies.sort(() => 0.5 - Math.random());
-    setCompanies(shuffled.slice(0, 3));
-  };
 
   return (
     <div className="company-dialog-backdrop">
       <div className="company-dialog-box">
-        {companies.map((company, index) => (
+        {company.map((company, index) => (
           <button key={index} className="company-btn" onClick={() => console.log(company)}>
             {company}
           </button>
