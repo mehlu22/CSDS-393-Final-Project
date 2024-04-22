f71030b3d34b07edc49861453e3f3ffca153fdef
GBK2002 <66865678+GBK2002@users.noreply.github.com>
2024-04-22 02:10:36 -0400

    formatting and functionality changes

--- frontend/src/CitiDialogBox/CitiDialogBox.js
+++ frontend/src/CitiDialogBox/CitiDialogBox.js
@@ -2,42 +2,33 @@ import React, { useState, useEffect } from 'react';
 import './CitiDialogBox.css';
 import CityDetailsDialogBox from '../CitiesDetailsDialogBox/CityDetailsDialogBox'; // This will be the new component
 
-function CitiesDialogBox({ cities, onCitySelect, onClose }) {
-    const [selectedCity, setSelectedCity] = useState('');
-    const [showCityDetails, setShowCityDetails] = useState(false);
+function CitiesDialogBox({cities, onCitySelect, onClose }) {
+  const [selectedCity, setSelectedCity] = useState('');
+  const [showCityDetails, setShowCityDetails] = useState(false);
 
 
 
-    const handleCityClick = (city) => {
-        onCitySelect(city);
-    };
+  const handleCityClick = (city) => {
+    onCitySelect(city);
+  };
 
-    const handleCloseCityDetails = () => {
-        setShowCityDetails(false);
-    };
+  const handleCloseCityDetails = () => {
+    setShowCityDetails(false);
+  };
 
-    return ( <
-        div className = "cities-dialog-backdrop" >
-        <
-        div className = "cities-dialog-box" >
-        <
-        h2 > Selected Cities < /h2> <
-        ul > {
-            cities.map((city, index) => ( <
-                li key = { index }
-                onClick = {
-                    () => handleCityClick(city)
-                } > { city } < /li>
-            ))
-        } <
-        /ul> <
-        button onClick = { onClose } > Close < /button> {
-        showCityDetails && < CityDetailsDialogBox city = { selectedCity }
-        onClose = { handleCloseCityDetails }
-        />} < /
-        div > <
-        /div>
-    );
+  return (
+    <div className="cities-dialog-backdrop">
+      <div className="cities-dialog-box">
+        <h2>Selected Cities</h2>
+        <ul>
+          {cities.map((city, index) => (
+            <li key={index} onClick={() => handleCityClick(city)}>{city}</li>
+          ))}
+        </ul>
+        <button onClick={onClose}>Close</button>
+      </div>
+    </div>
+  );
 }
 
 export default CitiesDialogBox;
