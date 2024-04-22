77eaf80ef35b07a22d17dd64a697314669e111ef
GBK2002 <66865678+GBK2002@users.noreply.github.com>
2024-04-21 21:07:19 -0400

    update city dialog box for company update

--- frontend/src/CitiDialogBox/CitiDialogBox.js
+++ frontend/src/CitiDialogBox/CitiDialogBox.js
@@ -2,15 +2,14 @@ import React, { useState, useEffect } from 'react';
 import './CitiDialogBox.css';
 import CityDetailsDialogBox from '../CitiesDetailsDialogBox/CityDetailsDialogBox'; // This will be the new component
 
-function CitiesDialogBox({ cities, onClose }) {
+function CitiesDialogBox({cities, onCitySelect, onClose }) {
   const [selectedCity, setSelectedCity] = useState('');
   const [showCityDetails, setShowCityDetails] = useState(false);
 
 
 
   const handleCityClick = (city) => {
-    setSelectedCity(city);
-    setShowCityDetails(true);
+    onCitySelect(city);
   };
 
   const handleCloseCityDetails = () => {
