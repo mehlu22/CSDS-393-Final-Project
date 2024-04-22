acaeec7611bc7c31518920ded70f3f33634c08c1
GBK2002 <66865678+GBK2002@users.noreply.github.com>
2024-04-22 02:32:48 -0400

    Displaying Neighborhood List updated

--- frontend/src/NeighborhoodListDialogBox/NeighborhoodListDialogBox.js
+++ frontend/src/NeighborhoodListDialogBox/NeighborhoodListDialogBox.js
@@ -1,24 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import './NeighborhoodListDialogBox.css'; // Make sure to create and link this CSS file
 
-function NeighborhoodListDialogBox({ onClose }) {
-  const [neighborhoods, setNeighborhoods] = useState([]);
+function NeighborhoodListDialogBox({ neighborhoods, onClose }) {
+  const [neighborhoodList, setNeighborhoodList] = useState([]);
 
   useEffect(() => {
-    pickRandomNeighborhoods();
+    setNeighborhoodList(neighborhoods)
   }, []);
 
-  const pickRandomNeighborhoods = () => {
-    const allNeighborhoods = ['University Circle', 'Euclid', 'Hessler', 'Tropicana', 'Mullhood'];
-    setNeighborhoods(allNeighborhoods.sort(() => 0.5 - Math.random()).slice(0, 3));
-  };
+
 
   return (
     <div className="neighborhood-list-dialog-backdrop">
       <div className="neighborhood-list-dialog-box">
         <h2>Selected Neighborhoods</h2>
         <ul>
-          {neighborhoods.map((neighborhood, index) => (
+          {neighborhoodList.map((neighborhood, index) => (
             <li key={index}>{neighborhood}</li>
           ))}
         </ul>
